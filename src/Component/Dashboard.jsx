405c28ff18d5ad2ce5dd730eb47fa74f9a6d67f9
mohammadashrafulislam <ixdell41@gmail.com>
2024-09-24 14:46:24 +0600

    removed some btns frm dashboard

--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -328,15 +328,6 @@ const Dashboard = () => {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
               >
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
                 <li onClick={handleLogOut}>
                   <a>Logout</a>
                 </li>
